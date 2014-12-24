086d5cc9211c061325f0de5de520277659b289b8
xuhuan <lanjingling100@163.com>
2014-12-24 14:32:25 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,21 @@
+gulp-seajs-config
+=================
+生成seajs-config 的 map 参数的js文件，因为spm-build没有生成map的功能，所以写了这一个。
+
+Usage
+```
+var gulp = require('gulp');
+var gsc = require('gulp-seajs-config');
+
+gulp.task('default', function() {
+	return gulp.src('./spm-modules/**/*.js')
+		.pipe(gsc({
+			target: './seajs-config.js',
+			cwd: './spm-modules'
+		}))
+		.pipe(gulp.dest('./dest'));
+});
+```
+License
+
+MIT
---
+++ index.js
@@ -0,0 +1,51 @@
+"use strict";
+var crypto = require("crypto"),
+	through = require("through2"),
+	path = require("path"),
+	File = require("gulp-util").File;
+
+module.exports = function(opts) {
+	opts = typeof(opts) === "string" ? {
+		target: opts
+	} : (opts || {});
+	opts.target = opts.target || "./seajs-config.js";
+	opts.algorithm = opts.algorithm || "sha1";
+	opts.length = opts.length || null;
+	opts.flatten = opts.flatten === true;
+	opts.cwd = opts.cwd || process.cwd();
+
+	var map = {};
+
+	if (!map.hasOwnProperty(opts.target)) {
+		map[opts.target] = {
+			'map': []
+		};
+	}
+
+	var getHashName = function(file) {
+		if (file.isNull()) return '';
+		var contents = file.isBuffer() ? file.contents : new Buffer();
+		if (file.isStream()) file.contents.pipe(contents);
+		var digest = crypto.createHash(opts.algorithm);
+		digest.update(file.contents);
+		var tag = digest.digest("hex");
+		var name = opts.flatten ? path.basename(file.path) : path.relative(opts.cwd, file.path).replace(/\\/g, '/');
+		return [name, name + ' ? ' + (opts.length ? tag.substr(0, opts.length) : tag)];
+	}
+
+	var stream = through.obj(function(file, enc, done) {
+		if (file.isNull()) return;
+		map[opts.target].map.push(getHashName(file));
+		done();
+	}, function(done) {
+		this.push(new File({
+			cwd: opts.cwd,
+			base: opts.cwd,
+			path: path.join(opts.cwd, opts.target),
+			contents: new Buffer('seajs.config(' + JSON.stringify(map[opts.target]) + ');')
+		}));
+		done();
+	});
+
+	return stream;
+};
